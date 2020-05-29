49d410967e2fa8551081bc85116f57c720801843
Juggernaut9434 <juggernaut9434@gmail.com>
2020-05-28 20:48:21 -0700

    hello world

---
+++ src/helloworld.java
@@ -0,0 +1,9 @@
+// if src is not blue, right click go down to bottom, hover over Mark Directory as, and choose Sources Root
+
+public class helloworld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
